f15e0756922fe71ced12b1cdb828f20ca3433c9c
parinithshekar <parinith.1114@gmail.com>
2020-05-08 00:38:03 +0530

    Fixed login to docker

--- makefile-plugins/docker.mk
+++ makefile-plugins/docker.mk
@@ -1,6 +1,10 @@
 .DEFAULT_GOAL := help
 SHELL := /bin/bash
 
+.PHONY: docker-login
+docker-login: # Login to dockerhub account
+	echo "$${DOCKER_PASS}" | docker login --username "$${DOCKER_USER}" --password-stdin
+
 .PHONY: docker-local
 docker-local:  # Build a docker image for local development.
 	docker build -t $(REPO_NAME):$(USER) .
@@ -11,7 +15,7 @@ docker-build:  # Build a docker image without pushing it out.
 	docker build -t $(DOCKER_USER)/$(REPO_NAME):$(DOCKER_TAG) .
 
 .PHONY: docker-push
-docker-push:  # Build a docker image without pushing it out.
+docker-push:  # Push docker image to dockerhub
 	docker push $(DOCKER_USER)/$(REPO_NAME):$(DOCKER_TAG)
 
 .PHONY: docker-all
